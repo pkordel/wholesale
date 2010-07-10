28982ac0ca1184206035d240042e39797a450303
Peter Kordel <pkordel@gmail.com>
2010-07-10 16:13:15 +0200

    fixed table check

--- vendor/extensions/site/lib/site/product.rb
+++ vendor/extensions/site/lib/site/product.rb
@@ -1,8 +1,8 @@
 module Site::Product
   def self.included(model)
     model.class_eval do
-      if ActiveRecord::Base.connection.table_exists?(Variant.to_s)
-        delegate_belongs_to :master, :wholesale_price_usd if Variant.column_names.include?("wholesale_price_usd")
+      if ActiveRecord::Base.connection.table_exists?('variants')
+        delegate_belongs_to :master, :wholesale_price_usd if Variant.column_names.include?('wholesale_price_usd')
       end
     end
   end
